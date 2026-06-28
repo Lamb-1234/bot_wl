fffac05c09d60df892f38699ba28409e33c6b0df
Lamb <rafaelaugustolamb@gmail.com>
2026-06-28 14:19:36 -0300

    Update interactionCreate.js

--- events/interactionCreate.js
+++ events/interactionCreate.js
@@ -1,1 +1,19 @@
+const whitelist = require("../handlers/whitelist");
 
+module.exports = {
+
+    name: "interactionCreate",
+
+    async execute(interaction) {
+
+        if (interaction.isButton()) {
+            return whitelist.button(interaction);
+        }
+
+        if (interaction.isModalSubmit()) {
+            return whitelist.modal(interaction);
+        }
+
+    }
+
+};
