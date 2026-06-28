4967b04e51e40c53209cb85d6fc5e665b8e0dcb0
Lamb <rafaelaugustolamb@gmail.com>
2026-06-28 15:21:38 -0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,12 +1,18 @@
+const config = require("../config/config");
+
 module.exports = {
     name: "guildMemberAdd",
 
     async execute(member) {
 
-        const role = member.guild.roles.cache.get(process.env.OLHEIRO_ID);
+        const role = member.guild.roles.cache.get(config.ROLES.OLHEIRO);
 
-        if (role) {
+        if (!role) return;
+
+        try {
             await member.roles.add(role);
+        } catch (err) {
+            console.log("Erro ao dar cargo OLHEIRO:", err);
         }
 
     }
