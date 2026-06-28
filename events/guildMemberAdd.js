95b6e4b60244d17c4292b9b11f1304ab34beb399
Lamb <rafaelaugustolamb@gmail.com>
2026-06-28 14:19:02 -0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,1 +1,13 @@
+module.exports = {
+    name: "guildMemberAdd",
 
+    async execute(member) {
+
+        const role = member.guild.roles.cache.get(process.env.OLHEIRO_ID);
+
+        if (role) {
+            await member.roles.add(role);
+        }
+
+    }
+};
