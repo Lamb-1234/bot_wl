955dbbe8fa10f18554b655594fdbd3a6b28daf02
Lamb <rafaelaugustolamb@gmail.com>
2026-06-28 14:51:28 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,1 +1,9 @@
-aaaaa
+module.exports = {
+
+    OLHEIRO_ID: process.env.OLHEIRO_ID,
+    MEMBRO_ID: process.env.MEMBRO_ID,
+    ADMIN_ID: process.env.ADMIN_ID,
+    SETUP_ID: process.env.SETUP_ID,
+    DISCORD_ID: process.env.DISCORD_ID
+
+}
