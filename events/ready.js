501cedfd8b127433747818115f985bcccda69aad
Lamb <rafaelaugustolamb@gmail.com>
2026-06-28 14:18:43 -0300

    Update ready.js

--- events/ready.js
+++ events/ready.js
@@ -1,1 +1,12 @@
+const whitelist = require("../handlers/whitelist");
 
+module.exports = {
+    name: "ready",
+    once: true,
+
+    async execute(client) {
+        console.log(`Bot online: ${client.user.tag}`);
+
+        whitelist.sendPanel(client);
+    }
+};
