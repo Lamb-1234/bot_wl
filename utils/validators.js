aae87943e191f96681ed7afa57035d51e441463e
Lamb <rafaelaugustolamb@gmail.com>
2026-06-28 14:56:46 -0300

    Update validators.js

--- utils/validators.js
+++ utils/validators.js
@@ -1,1 +1,32 @@
+function validateName(name) {
 
+    if (!name) {
+        return "Informe um nome.";
+    }
+
+    if (name.length > 100) {
+        return "O nome pode ter no máximo 100 caracteres.";
+    }
+
+    const parts = name.trim().split(/\s+/);
+
+    if (parts.length < 2) {
+        return "Informe nome e sobrenome.";
+    }
+
+    return null;
+}
+
+function validateId(id) {
+
+    if (!/^\d+$/.test(id)) {
+        return "O ID deve conter apenas números.";
+    }
+
+    return null;
+}
+
+module.exports = {
+    validateName,
+    validateId
+};
