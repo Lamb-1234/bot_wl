5d7f2fab0b96046f9664d1aa75e906feb0f259ca
Lamb <rafaelaugustolamb@gmail.com>
2026-06-28 15:37:00 -0300

    Update wlStore.js

--- data/wlStore.js
+++ data/wlStore.js
@@ -1,1 +1,38 @@
+const wlUsers = new Map();
 
+/*
+status:
+pending
+approved
+rejected
+*/
+
+function hasWL(userId) {
+    return wlUsers.has(userId);
+}
+
+function getWL(userId) {
+    return wlUsers.get(userId);
+}
+
+function createWL(userId, data) {
+    wlUsers.set(userId, {
+        status: "pending",
+        ...data
+    });
+}
+
+function updateWL(userId, data) {
+    if (!wlUsers.has(userId)) return;
+    wlUsers.set(userId, {
+        ...wlUsers.get(userId),
+        ...data
+    });
+}
+
+module.exports = {
+    hasWL,
+    getWL,
+    createWL,
+    updateWL
+};
