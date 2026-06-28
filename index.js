9b39909180cb4e04633476d275f62dd62cceb437
Lamb <rafaelaugustolamb@gmail.com>
2026-06-28 14:57:46 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,23 @@
 require("dotenv").config();
 
-const { Client, GatewayIntentBits } = require("discord.js");
+const express = require("express");
 const fs = require("fs");
+const path = require("path");
+const { Client, GatewayIntentBits } = require("discord.js");
+
+const config = require("./config/config");
+
+const app = express();
+
+app.get("/", (_, res) => {
+    res.send("Bot online.");
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Servidor HTTP iniciado (${PORT})`);
+});
 
 const client = new Client({
     intents: [
@@ -12,16 +28,22 @@ const client = new Client({
     ],
 });
 
-const eventFiles = fs.readdirSync("./events").filter(file => file.endsWith(".js"));
+console.log("Iniciando bot...");
+
+const eventsPath = path.join(__dirname, "events");
+
+const eventFiles = fs.readdirSync(eventsPath).filter(file => file.endsWith(".js"));
 
 for (const file of eventFiles) {
-    const event = require(`./events/${file}`);
+
+    const event = require(path.join(eventsPath, file));
 
     if (event.once) {
         client.once(event.name, (...args) => event.execute(...args, client));
     } else {
         client.on(event.name, (...args) => event.execute(...args, client));
     }
+
 }
 
-client.login(process.env.DISCORD_ID);
+client.login(config.TOKEN);
