bd6c60846ef86ca3dcb3c8bc60cf2044c0ca87ef
Lamb <rafaelaugustolamb@gmail.com>
2026-06-28 14:56:06 -0300

    Update config.js

--- config/config.js
+++ config/config.js
@@ -1,9 +1,23 @@
 module.exports = {
+    TOKEN: process.env.TOKEN,
 
-    OLHEIRO_ID: process.env.OLHEIRO_ID,
-    MEMBRO_ID: process.env.MEMBRO_ID,
-    ADMIN_ID: process.env.ADMIN_ID,
-    SETUP_ID: process.env.SETUP_ID,
-    DISCORD_ID: process.env.DISCORD_ID
+    ROLES: {
+        OLHEIRO: process.env.OLHEIRO_ID,
+        MEMBRO: process.env.MEMBRO_ID,
+        ADMIN: process.env.ADMIN_ID,
+    },
 
-}
+    CHANNELS: {
+        WL_PANEL: process.env.SETUP_ID,
+    },
+
+    COLORS: {
+        PRIMARY: 0x5865F2,
+        SUCCESS: 0x57F287,
+        ERROR: 0xED4245,
+        WARNING: 0xFEE75C,
+        INFO: 0x3498DB,
+    },
+
+    EMBED_FOOTER: "Rocinha RP • Sistema de Whitelist"
+};
