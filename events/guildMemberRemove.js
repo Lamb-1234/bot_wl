cdeb1d801234b208b475c1bfe2f8120a65d2285d
Lamb <rafaelaugustolamb@gmail.com>
2026-06-29 00:05:01 -0300

    Update guildMemberRemove.js

--- events/guildMemberRemove.js
+++ events/guildMemberRemove.js
@@ -9,11 +9,13 @@ module.exports = {
         const wl = wlStore.getWL(member.id);
         if (!wl) return;
 
+        // remove WL automaticamente ao sair
         wlStore.deleteWL(member.id);
 
         const channel = member.guild.channels.cache.get(config.CHANNELS.WL_LOGS);
-        if (!channel) return;
 
-        channel.send(`⚠ WL removida automaticamente: **${member.user.tag}** saiu do servidor.`);
+        if (channel) {
+            channel.send(`⚠ WL removida automaticamente: **${member.user.tag}** saiu do servidor.`);
+        }
     }
 };
