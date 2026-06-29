389cbbd19f18a189e60c1ade401f3000c08ee4c1
Lamb <rafaelaugustolamb@gmail.com>
2026-06-29 00:04:05 -0300

    Update guildMemberAdd.js

--- events/guildMemberAdd.js
+++ events/guildMemberAdd.js
@@ -1,19 +1,23 @@
 const config = require("../config/config");
+const wlStore = require("../data/wlStore");
 
 module.exports = {
     name: "guildMemberAdd",
 
     async execute(member) {
 
-        const role = member.guild.roles.cache.get(config.ROLES.OLHEIRO);
+        const wl = wlStore.getWL(member.id);
 
+        // Se já tem WL aprovada, NÃO dá olheiro
+        if (wl && wl.status === "approved") return;
+
+        const role = member.guild.roles.cache.get(config.ROLES.OLHEIRO);
         if (!role) return;
 
         try {
             await member.roles.add(role);
         } catch (err) {
-            console.log("Erro ao dar cargo OLHEIRO:", err);
+            console.log("Erro ao dar cargo OLHEIRO:", err.message);
         }
-
     }
 };
