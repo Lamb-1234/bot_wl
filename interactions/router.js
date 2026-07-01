80d39ae1e1b14fdac476a62fd4856e0e3ac04c5e
Lamb <rafaelaugustolamb@gmail.com>
2026-06-30 23:10:37 -0300

    Update router.js

--- interactions/router.js
+++ interactions/router.js
@@ -1,3 +1,11 @@
+const config = require("../config/config");
+const { canHandleWL } = require("../utils/permissions");
+
+const handleRequestButton = require("./buttons/requestWL");
+const handleWhitelistModal = require("./modals/whitelistModal");
+const handleApproveButton = require("./buttons/approveWL");
+const handleRejectButton = require("./buttons/rejectWL");
+
 module.exports = async (interaction, client) => {
 
     console.log(
@@ -9,15 +17,14 @@ module.exports = async (interaction, client) => {
 
     try {
 
-        // Botão solicitar WL
         if (
             interaction.isButton() &&
             interaction.customId === config.BUTTONS.REQUEST_WL
-        )  {
-    console.log("Entrou no botão WL");
-    return handleRequestButton(interaction);
+        ) {
+            console.log("Entrou no botão WL");
+            return handleRequestButton(interaction);
         }
-        // Modal
+
         if (
             interaction.isModalSubmit() &&
             interaction.customId === "wl_modal"
@@ -25,7 +32,6 @@ module.exports = async (interaction, client) => {
             return handleWhitelistModal(interaction);
         }
 
-        // Staff
         if (interaction.isButton()) {
 
             const [action] = interaction.customId.split(":");
@@ -38,12 +44,10 @@ module.exports = async (interaction, client) => {
             }
 
             if (!canHandleWL(interaction.member)) {
-
                 return interaction.reply({
                     content: "❌ Você não possui permissão.",
                     ephemeral: true
                 });
-
             }
 
             if (action === config.BUTTONS.ACCEPT) {
@@ -59,12 +63,10 @@ module.exports = async (interaction, client) => {
         console.error(error);
 
         if (!interaction.replied && !interaction.deferred) {
-
             return interaction.reply({
                 content: "❌ Ocorreu um erro inesperado.",
                 ephemeral: true
             });
-
         }
 
     }
