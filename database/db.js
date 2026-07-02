1cace06c8457d8776e3540059501d227e7baea3e
Lamb <rafaelaugustolamb@gmail.com>
2026-07-01 22:52:02 -0300

    Update db.js

--- database/db.js
+++ database/db.js
@@ -1,6 +1,14 @@
 const sqlite3 = require("sqlite3").verbose();
 
-const db = new sqlite3.Database("./wl.db");
+const db = new sqlite3.Database("./wl.db", (err) => {
+
+    if (err) {
+        console.error(err);
+    } else {
+        console.log("SQLite conectado.");
+    }
+
+});
 
 // cria tabela
 db.serialize(() => {
@@ -12,7 +20,37 @@ db.serialize(() => {
             status TEXT,
             messageId TEXT
         )
-    `);
-});
+`, (err)=>{
 
+    if(err){
+        console.error(err);
+    }else{
+        console.log("Tabela criada.");
+    }
+    );
+
+    db.run(
+    "INSERT INTO whitelist(userId, nome, idJogador, status, messageId) VALUES(?, ?, ?, ?, ?)",
+    ["1230123", "Ricardo", "1331313", "online", "1521995023430717533"],
+    function(err){
+
+        if(err){
+            console.error(err);
+        }else{
+            console.log("Registro inserido:", this.lastID);
+        }
+
+    }
+    );
+    db.all("SELECT * FROM whitelist", [], (err, rows)=>{
+
+    if(err){
+        console.error(err);
+    }else{
+        console.log(rows);
+    }
+
+    });
+});
+    
 module.exports = db;
